886293ca753606b2b839aee070cc8ca6e88d12ae
RavenCheng1120 <ravencheng66@gmail.com>
2020-07-10 01:21:21 +0800

    windows version

---
+++ RaceCarSpeed/ImageProcess.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace RaceCarSpeed
+{
+    public class ImageProcess
+    {
+        public readonly string filePath;
+        private readonly string filename;
+        public Bitmap processedImage;
+
+        public ImageProcess(string imageName)
+        {
+            // 取得測試圖片的path
+            // Debug狀態，image檔案位於/Users/patty/Projects/RaceCarSpeed/RaceCarSpeed/bin/Debug/netcoreapp3.1
+            filePath = Directory.GetCurrentDirectory();
+            filename = filePath + imageName;
+        }
+
+        public Bitmap ReadImage()
+        {
+            // 檢查檔案是否存在
+            if (!File.Exists(this.filename))
+            {
+                throw new FileNotFoundException(filename);
+            }
+
+            // 讀取圖片
+            processedImage = NegativePicture(new Bitmap(filename));
+            // 儲存圖片
+            // processedImage.Save(filePath + "/processedImage.png", ImageFormat.Png);
+
+            return processedImage;
+        }
+
+        // 將圖片轉換成負片效果
+        private static Bitmap NegativePicture(Bitmap image)
+        {
+            int w = image.Width;
+            int h = image.Height;
+            BitmapData srcData = image.LockBits(new Rectangle(0, 0, w, h),
+            ImageLockMode.ReadOnly, PixelFormat.Format32bppArgb);
+            int bytes = srcData.Stride * srcData.Height;
+            byte[] buffer = new byte[bytes];
+            byte[] result = new byte[bytes];
+            Marshal.Copy(srcData.Scan0, buffer, 0, bytes);
+            image.UnlockBits(srcData);
+            int cChannels = 3;
+            for (int y = 0; y < h; y++)
+            {
+                for (int x = 0; x < w; x++)
+                {
+                    int current = y * srcData.Stride + x * 4;
+                    for (int c = 0; c < cChannels; c++)
+                    {
+                        result[current + c] = (byte)(255 - buffer[current + c]);
+                    }
+                    result[current + 3] = 255;
+                }
+            }
+
+            Bitmap resImg = new Bitmap(w, h);
+            BitmapData resData = resImg.LockBits(new Rectangle(0, 0, w, h),
+            ImageLockMode.WriteOnly, PixelFormat.Format32bppArgb);
+            Marshal.Copy(result, 0, resData.Scan0, bytes);
+            resImg.UnlockBits(resData);
+            return resImg;
+        }
+    }
+}
+
---
+++ RaceCarSpeed/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Drawing;
+using Tesseract;
+
+namespace RaceCarSpeed
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // image processing
+            ImageProcess imageProcess = new ImageProcess("/rawImage.png");
+            Bitmap finalImage = imageProcess.ReadImage();
+            finalImage.Save(imageProcess.filePath + "/processedImage.png", System.Drawing.Imaging.ImageFormat.Png);
+
+            // use Tesseract to recognize number
+            try
+            {
+                TesseractEngine ocr = new TesseractEngine("./tessdata", "eng", EngineMode.Default);
+                // Pix pixImage = PixConverter.ToPix(finalImage);
+                Pix pixImage = Pix.LoadFromFile(imageProcess.filePath + "/processedImage.png");
+                Page page = ocr.Process(pixImage);
+                string str = page.GetText();//識別後的內容
+                Console.WriteLine("Result: " + str);
+                page.Dispose();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("error message");
+                Console.WriteLine(ex);
+            }
+
+
+
+        }
+    }
+}
